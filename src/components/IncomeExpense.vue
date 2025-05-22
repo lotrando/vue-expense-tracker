eb9444dee07c69bcbbcbc0ee8989327fba894a64
Lotrando <miroslav.klika@seznam.cz>
2025-05-23 00:46:22 +0200

    Small vue expense tracker v1.0

---
+++ src/components/IncomeExpense.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="inc-exp-container">
+    <div>
+      <h4>Incomes</h4>
+      <p class="money plus" id="money-plus">€{{ incomes }}</p>
+    </div>
+    <div>
+      <h4>Expenses</h4>
+      <p class="money minus" id="money-minus">€{{ expenses }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    incomes: {
+      type: Number,
+      required: true
+    },
+    expenses: {
+      type: Number,
+      required: true
+    }
+  });
+</script>
+
+<style scoped></style>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,30 @@
+  <template>
+    <h3>Transactions history</h3>
+    <ul id="list" class="list">
+      <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+        {{ transaction.text }} <span>{{ transaction.amount }}</span>
+        <button @click="deleteTransaction(transaction.id)" class="delete-btn">
+          <DeleteIcon />
+        </button>
+      </li>
+    </ul>
+  </template>
+
+<script setup>
+  import DeleteIcon from './DeleteIcon.vue';
+
+  const emit = defineEmits(['transactionDeleted']);
+
+  defineProps({
+    transactions: {
+      type: Array,
+      required: true
+    }
+  });
+
+  const deleteTransaction = (id) => {
+    emit('transactionDeleted', id);
+  };
+</script>
+
+<style scoped></style>

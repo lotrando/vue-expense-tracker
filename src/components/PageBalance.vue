31e7e4835a0f78b8a5deba3f83373b7bda9e59a7
Lotrando <miroslav.klika@seznam.cz>
2025-05-23 01:14:56 +0200

    Fix

--- src/components/PageBalance.vue
+++ src/components/PageBalance.vue
@@ -1,20 +1,15 @@
 <template>
   <h4>Your Balance</h4>
-  <h1 id="balance">
-    €
-    <Transition name="fade" mode="out-in">
-      <span :key="total">
-        {{ total.toLocaleString('cs-CZ', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}
-      </span>
-    </Transition>
+  <h1 id="balance">€
+    {{ total.toLocaleString('cs-CZ', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}
   </h1>
 </template>
 
 <script setup>
-defineProps({
-  total: {
-    type: Number,
-    required: true,
-  },
-})
+  defineProps({
+    total: {
+      type: Number,
+      required: true,
+    },
+  })
 </script>

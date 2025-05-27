4bfd74a4c35a50cc2cea19d72aceacced4b2d035
Lotrando <miroslav.klika@seznam.cz>
2025-05-27 15:01:08 +0200

    css update and vue scroll

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,34 +1,41 @@
 <template>
   <h3>Transactions history</h3>
-  <ul id="list" class="list">
-    <li
-      v-for="transaction in transactions"
-      :key="transaction.id"
-      :class="transaction.amount < 0 ? 'minus' : 'plus'"
-    >
-      {{ transaction.text }} <span>{{ transaction.amount }}</span>
-      <button @click="deleteTransaction(transaction.id)" class="delete-btn">
-        <DeleteIcon />
-      </button>
-    </li>
-  </ul>
+  <PerfectScrollbar>
+    <ul id="list" class="list">
+      <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+        {{ transaction.text }} <span>{{ transaction.amount }}</span>
+        <button @click="deleteTransaction(transaction.id)" class="delete-btn">
+          <DeleteIcon />
+        </button>
+      </li>
+    </ul>
+  </PerfectScrollbar>
 </template>
 
 <script setup>
-import DeleteIcon from './DeleteIcon.vue'
+  import DeleteIcon from './DeleteIcon.vue'
+  import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
 
-const emit = defineEmits(['transactionDeleted'])
+  const emit = defineEmits(['transactionDeleted'])
 
-defineProps({
-  transactions: {
-    type: Array,
-    required: true,
-  },
-})
+  defineProps({
+    transactions: {
+      type: Array,
+      required: true,
+    },
+  })
 
-const deleteTransaction = (id) => {
-  emit('transactionDeleted', id)
-}
+  const deleteTransaction = (id) => {
+    emit('transactionDeleted', id)
+  }
 </script>
 
-<style scoped></style>
+<style>
+  @import 'vue3-perfect-scrollbar/style.css';
+
+  .ps {
+    max-height: 200px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+</style>
